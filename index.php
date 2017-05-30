2a57a06f5705b56dbb44f8fcdbfd97a026c2a2d0
Andrey <smirnovdel@mail.ru>
2017-05-30 13:46:35 +0300

    ДЗ от 29.5

--- index.php
+++ index.php
@@ -13,27 +13,62 @@
                 <li>
                 <label for="food1">блюдо 1</label> <br>   
                 <input name="food1" type="text"/>
+				<label> Кол-во:<select name="schet1">
+                    <option >1</option>
+                    <option >2</option>
+                    <option >3</option>
+					<option >4</option>
+					<option >5</option>					
+                  </select>шт.</label>
                 </li>
                 
                 <li>
                 <label for="food2">блюдо 2</label> <br>   
                 <input name="food2" type="text"/>
+					<label> Кол-во:<select name="schet2">
+                    <option >1</option>
+                    <option >2</option>
+                    <option >3</option>
+					<option >4</option>
+					<option >5</option>					
+                  </select>шт.</label>
                 </li>
                 
                 <li>
                 <label for="food3">блюдо 3</label> <br>   
                 <input name="food3" type="text"/>
+				<label> Кол-во:<select name="schet3">
+                    <option >1</option>
+                    <option >2</option>
+                    <option >3</option>
+					<option >4</option>
+					<option >5</option>					
+                  </select>шт.</label>
                 </li>
                 
                                 
                 <li>
                 <label for="food4">блюдо 4</label> <br>   
                 <input name="food4" type="text"/>
+				<label> Кол-во:<select name="schet4">
+                    <option >1</option>
+                    <option >2</option>
+                    <option >3</option>
+					<option >4</option>
+					<option >5</option>					
+                  </select>шт. </label>
                 </li>
                 
                 <li>
                 <label for="food5">блюдо 5</label> <br>   
                 <input name="food5" type="text"/>
+				<label> Кол-во:<select name="schet5">
+                    <option >1</option>
+                    <option >2</option>
+                    <option >3</option>
+					<option >4</option>
+					<option >5</option>					
+                  </select>шт.</label>
                 </li>
                   
                   <li>
@@ -53,28 +88,28 @@
                  <ul>
                 <li>
                 <label for="price1">цена 1</label> <br>   
-                <input name="price1" type="text"/>
+                <label><input name="price1" type="text"/>руб.</label>
                 </li>
                 
                 <li>
                 <label for="price2">цена 2</label> <br>   
-                <input name="price2" type="text"/>
+                <label><input name="price2" type="text"/>руб.</label>
                 </li>
                 
                 <li>
                 <label for="price3">цена 3</label> <br>   
-                <input name="price3" type="text"/>
+                <label><input name="price3" type="text"/>руб.</label>
                 </li>
                 
                                 
                 <li>
                 <label for="price4">цена 4</label> <br>   
-                <input name="price4" type="text"/>
+                <label><input name="price4" type="text"/>руб.</label>
                 </li>
                 
                 <li>
                 <label for="price5">цена 5</label> <br>   
-                <input name="price5" type="text"/>
+                <label><input name="price5" type="text"/>руб.</label>
                 </li>
                 
                 <li>
--- result.php
+++ result.php
@@ -3,13 +3,15 @@ $sum=0;
 $food = array();
 $price = array();
 for ($i=1;$i<6;$i++) {
-    $food[$i]=$_POST['food'.$i];
-    $price[$i]=$_POST['price'.$i];
-    $sum+=$price[$i];
-    $mealtime=$_POST['mealtime'];
-    };
+	if (isset($_POST['food'.$i]) && isset($_POST['price'.$i])){
+    	$food[$i]=$_POST['food'.$i];
+    	$price[$i]=$_POST['price'.$i]*$_POST['schet'.$i];
+		if ($food[$i]=='' || $price[$i]==0){unset($food[$i]); unset($price[$i]);}
+		$sum+=$price[$i];		
+    	}
+$mealtime=$_POST['mealtime'];
+ };
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -22,9 +24,10 @@ for ($i=1;$i<6;$i++) {
             <div class="left">
                 <ul>
                     <?php
- 						for ($i=1;$i<6;$i++){   
-						echo "<li>  $food[$i] </li> ";
-											};
+ 						for ($i=1;$i<6;$i++){  
+							if (isset($food[$i])){
+							echo "<li>  $food[$i] </li> ";}
+						};
 						echo "<li>  $mealtime	</li>";
 					?>
                </ul>
@@ -32,10 +35,37 @@ for ($i=1;$i<6;$i++) {
         <div class="right">
                 <ul>
                     <?php
-						for ($i=1;$i<6;$i++){   
-						echo "<li>  $price[$i] </li> ";
+						for ($i=1;$i<6;$i++){  
+							if (isset($price[$i])){
+								echo "<li> Цена ". $price[$i]." руб. </li> ";}
 											};
-					echo "<li>  $sum	</li>";
+					echo "<li> Ваш заказ ". $sum	." руб.</li>";		
+					
+					if($sum>=100 && $sum<150){
+							$sale=$sum/100*5;
+							if($sum-$sale<100){
+								echo "<li> Ваша скидка ". ($sum-100)	." руб.</li>";
+								echo "<li> к оплате 100 руб.</li>";}
+							else{echo "<li> Ваша скидка ". $sale	." руб.</li>";
+								echo "<li> к оплате ".($sum-$sale). " руб.</li>";}}
+					elseif($sum>=150 && $sum<200){
+							$sale=$sum/100*7;							
+							if($sum-$sale<150){
+								echo "<li> Ваша скидка ". ($sum-150)	." руб.</li>";
+								echo "<li> к оплате 150 руб.</li>";}
+							else{echo "<li> Ваша скидка ". $sale	." руб.</li>";
+								echo "<li> к оплате ".($sum-$sale). " руб.</li>";}}
+					elseif($sum>=200){
+							$sale=$sum/100*10;							
+							if($sum-$sale<200){
+								echo "<li> Ваша скидка ". ($sum-200)	." руб.</li>";
+								echo "<li> к оплате 200 руб.</li>";}
+							else{echo "<li> Ваша скидка ". $sale	." руб.</li>";
+								echo "<li> к оплате ".($sum-$sale). " руб.</li>";}}
+					elseif($sum<100){
+							echo "<li> к оплате ".$sum. " руб.</li>";
+						
+					}				
 					?>
                 </ul>
             </div>
