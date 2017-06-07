f6b1824657471d70b977d068a3c921742a3e0339
smirnovdel <smirnovdel@mail.ru>
2017-06-07 21:41:48 +0300

    ДЗ

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include('save/header.php')?>
+         <div class="left">
+			<?php include('save/nav.php');?>   
+		 </div>
+                
+         <div class="right">
+                 
+         </div>
+<?php include('save/footer.php')?>
