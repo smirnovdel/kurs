e764144160336dd36e7e3ffc3352e4c4ce6198a4
Andrey <smirnovdel@mail.ru>
2017-06-13 20:28:22 +0300

    ДЗ от 12.06

--- save/show_menu.php
+++ save/show_menu.php
@@ -1,31 +1,42 @@
 <?php include('db.php')?>
 <?php 
-$query="SELECT menu.id, food, weight, price, time_id FROM menu where time_id=1 ";
+	
+$query="SELECT table_time_id.id, title FROM table_time_id";
 $result = mysqli_query($db,$query);
-echo '<table> <th colspan="5" >Завтрак</th></tr> <tr><th>Блюдо</th><th>Вес</th><th>Цена</th></tr> <tr>';
 while($row = mysqli_fetch_array($result)){
-    printf( "<tr><td> %s </td>  <td>  %s </td> <td>  %s </td>  <td> <a href='edit_food.php?id=%s '>&#9998  </a> </td> <td><a href='all_menu.php?del_id=%s '> &#10007 </a></td></tr>"
-		  , $row['food'],$row['weight'],$row['price'],$row['id'],$row['id']);
-	$sum[]=$row['price'];
-}
-$query="SELECT menu.id, food, weight, price, time_id FROM menu where time_id=2 ";
-$result = mysqli_query($db,$query);
-echo '<tr><th colspan="5" >Обед</th></tr>';
-while($row = mysqli_fetch_assoc($result)){
-    printf( "<tr><td> %s </td>  <td>  %s </td> <td>  %s </td>  <td> <a href='edit_food.php?id=%s '>&#9998  </a> </td> <td><a href='all_menu.php?del_id=%s '> &#10007 </a></td></tr>"
-		  , $row['food'],$row['weight'],$row['price'],$row['id'],$row['id']);
-	$sum[]=$row['price'];
-}
-$query="SELECT menu.id, food, weight, price, time_id FROM menu where time_id=3 ";
-$result = mysqli_query($db,$query);
-echo '<tr><th colspan="5" >Ужин</th></tr>';
-while($row = mysqli_fetch_assoc($result)){
-    printf( "<tr><td> %s </td>  <td>  %s </td> <td>  %s </td>  <td> <a href='edit_food.php?id=%s '>&#9998  </a> </td> <td><a href='all_menu.php?del_id=%s '> &#10007 </a></td></tr>"
-		  , $row['food'],$row['weight'],$row['price'],$row['title'],$row['id']);
-	$sum[]=$row['price'];
-}
-echo'<tr><th>Итого</th><th> </th><th>'.array_sum($sum).'</th></tr> <tr>';
-echo '</table>';
-echo 'Кол-во блюд  '.count($sum);
+    $type_food[]=$row['title'];
+	$type_food_id[]=$row['id'];
+}	
+$id_count=count($type_food);
+$i=0;?>
+<form method="post" action="../save/show_zakaz.php">
+	<table>
+	<? while($i<$id_count):?>
+		<?$num=$type_food_id[$i];
+		$query="SELECT menu.id, food, weight, price, time_id FROM menu where time_id='$num' ";
+		$result = mysqli_query($db,$query);
+		echo '<th colspan="7" >'.$type_food[$i].'</th> <tr><th>Блюдо</th><th>Вес</th><th>Цена</th></tr> <tr>';?>
 
-	;?>
+	
+	<? while($row = mysqli_fetch_array($result)):?>
+				<tr><td> <?=$row['food']?> </td>  
+							<td> <?=$row['weight']?> </td>
+							<td>  <?=$row['price']?> </td>
+							<td> <a href='edit_food.php?id=<?=$row['id']?>'> &#9998  </a> </td>
+							<td><a href='all_menu.php?del_id=<?=$row['id']?>'> &#10007 </a></td>
+							<td><input type='checkbox' name='mass[]' value='<?=$row['id']?>'/></td>
+							<td><input type='number' name='<?=$row['id']?>'/></td></tr>
+			  				<? $sum[]=$row['price']; ?>
+				<? endwhile?>
+	
+	
+		<? $i++; ?>
+	<? endwhile?>
+	
+		<tr><th>Сумма</th><th> </th><th><? echo array_sum($sum) ?></th></tr>
+		
+	</table>
+	
+	<input type='submit' value="Сделать заказ">
+<p>Кол-во блюд  <? echo count($sum)?> </p>
+</form>
---
+++ save/show_zakaz.php
@@ -0,0 +1,57 @@
+<?php include('db.php')?>
+<?php include('header.php')?>
+
+
+	
+         <div class="left">
+		<? $query="SELECT table_time_id.id, title FROM table_time_id";
+		 $result = mysqli_query($db,$query);
+		/*Пересчет количества типов блюд. передача в массив*/
+		 while($row = mysqli_fetch_array($result)){
+			 $type_food[]=$row['title'];
+			 $type_food_id[]=$row['id'];
+			 }	
+		 $id_count=count($type_food);
+		 $i=0;?>
+		 <table>
+				 <!--перебирает все типы блюд имеющиеся в базе, делаем выборку всех записей по каждому типу блюд-->
+		<? while($i<$id_count):?>
+			<?$num=$type_food_id[$i];
+			 $j=$i;
+			 $query="SELECT menu.id, food, weight, price, time_id FROM menu where time_id='$num' ";
+			 $result = mysqli_query($db,$query);?>
+			 	<!--перебираем каждую запись выбранного типа-->
+				 <? while($row = mysqli_fetch_array($result)):?>
+			 	<!--перебираем каждую запись выбранного типа,если id данной записи есть в полученном массиве выводим-->
+				 	<? foreach($_POST['mass'] as $key => $value) :?>
+						<? if ($row['id']==$value):?>
+			 				<!--выводим заголовок если это первая запись в этом типе блюд-->
+				 			<?if ($j=$i) {echo '<th colspan="7" >'.$type_food[$i].'</th> <tr><th>Блюдо</th><th>Вес</th><th>Цена</th></tr> <tr>'; $j=0;}?>
+				 	 		<tr><td><?=$row['food']?> </td>  
+					 		<td> <?=$row['weight']?> гр. </td>
+					 		<td>  <?=$row['price']?> р. </td>
+					 		<td>  <?=$_POST[$value]?> </td>
+			 	 			</tr>
+				 			<? $sum[]=$row['price']*$_POST[$value]; ?>
+				 		<? endif?>
+		
+					<? endforeach ?>
+				 <? endwhile?>			
+		<? $i++; ?>
+		<? endwhile?>
+		<tr><th>Сумма</th><th> </th><th><? echo array_sum($sum) ?></th></tr>
+		
+		</table>
+			 
+			 
+			 
+		 </div>
+         <div class="right">
+           
+			 
+		  </div>
+<?php include('footer.php')?>
+
+
+
+
